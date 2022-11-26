b85a13ac140ba405fb5f47358a497dad144c7a0e
oumaima zineddine <oumaima1999asma@gmail.com>
2022-11-26 15:58:59 +0100

    tp2

--- oumaima.py
+++ oumaima.py
@@ -1,7 +1,12 @@
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
+from dotenv import load_dotenv
 
+import os
+load_dotenv()  
+#environment variables
+APIKEY = os.getenv("APIKEY")
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 parameters = {
     'start': '97',
@@ -11,7 +16,7 @@ parameters = {
 }
 headers = {
     'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': 'd53800e1-5c89-42d0-80ed-ebbf6e8fc2e7',
+    'X-CMC_PRO_API_KEY': APIKEY,
 }
 
 session = Session()
@@ -21,8 +26,9 @@ try:
     response = session.get(url, params=parameters)
     data = json.loads(response.text)
     json_string = json.dumps(data['data'])
-
+    
     print(json_string)
+
 except (ConnectionError, Timeout, TooManyRedirects) as e:
     print(e)
 
